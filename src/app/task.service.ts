05714ebd9220cde37c3b9a637cf45f42239c2115
Chaturvedi, Sumeet <sumeet_chaturvedi@optum.com>
2019-08-30 14:30:47 +0530

    Update Component
    Update Component

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Task } from './task.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,10 @@ export class TaskService {
   addOrUpdateTask(task: Task): void
   {
     console.log("Inside Service. The Data to be sent is "+ JSON.stringify(task));
-    this.http.post<Task>(this.apiUrl, task);
+    this.http.post<Task>(this.apiUrl + '/addtask', task);
+  }
+
+  viewTask(){
+    this.http.get<Task[]>(this.apiUrl + "viewtask").subscribe(data=>console.log(data));
   }
 }
